fa0cd93b43413fd651418e5931e3e7c1909dfe02
Colter Purcell <cpurce02@calpoly.edu>
2023-06-05 17:51:13 -0700

    Rough

--- matrix.c
+++ matrix.c
@@ -10,55 +10,58 @@
 #include <sys/types.h>
 #define MATRIX_DIMENSION_XY 10
 
-// SEARCH FOR TODO
+/* SEARCH FOR TODO */
 
-//************************************************************************************************************************
-// sets one element of the matrix
+/************************************************************************************************************************* */
+/* sets one element of the matrix */
 void set_matrix_elem(float *M, int x, int y, float f)
 {
     M[x + y * MATRIX_DIMENSION_XY] = f;
 }
-//************************************************************************************************************************
-// lets see it both are the same
+/************************************************************************************************************************* */
+/* lets see it both are the same */
 int quadratic_matrix_compare(float *A, float *B)
 {
-    for (int a = 0; a < MATRIX_DIMENSION_XY; a++)
-        for (int b = 0; b < MATRIX_DIMENSION_XY; b++)
+    int a, b;
+    for (a = 0; a < MATRIX_DIMENSION_XY; a++)
+        for (b = 0; b < MATRIX_DIMENSION_XY; b++)
             if (A[a + b * MATRIX_DIMENSION_XY] != B[a + b * MATRIX_DIMENSION_XY])
                 return 0;
 
     return 1;
 }
-//************************************************************************************************************************
-// print a matrix
+/************************************************************************************************************************* */
+/* print a matrix */
 void quadratic_matrix_print(float *C)
 {
+    int a, b;
     printf("\n");
-    for (int a = 0; a < MATRIX_DIMENSION_XY; a++)
+    for (a = 0; a < MATRIX_DIMENSION_XY; a++)
     {
         printf("\n");
-        for (int b = 0; b < MATRIX_DIMENSION_XY; b++)
+        for (b = 0; b < MATRIX_DIMENSION_XY; b++)
             printf("%.2f,", C[a + b * MATRIX_DIMENSION_XY]);
     }
     printf("\n");
 }
-//************************************************************************************************************************
-// multiply two matrices
+/************************************************************************************************************************* */
+/* multiply two matrices */
 void quadratic_matrix_multiplication(float *A, float *B, float *C)
 {
-    // nullify the result matrix first
-    for (int a = 0; a < MATRIX_DIMENSION_XY; a++)
-        for (int b = 0; b < MATRIX_DIMENSION_XY; b++)
+    int a, b, c;
+    /* nullify the result matrix first */
+    for (a = 0; a < MATRIX_DIMENSION_XY; a++)
+        for (b = 0; b < MATRIX_DIMENSION_XY; b++)
             C[a + b * MATRIX_DIMENSION_XY] = 0.0;
-    // multiply
-    for (int a = 0; a < MATRIX_DIMENSION_XY; a++)         // over all cols a
-        for (int b = 0; b < MATRIX_DIMENSION_XY; b++)     // over all rows b
-            for (int c = 0; c < MATRIX_DIMENSION_XY; c++) // over all rows/cols left
+    /* multiply */
+    for (a = 0; a < MATRIX_DIMENSION_XY; a++)         /* over all cols a */
+        for (b = 0; b < MATRIX_DIMENSION_XY; b++)     /* over all rows b */
+            for (c = 0; c < MATRIX_DIMENSION_XY; c++) /* over all rows/cols left */
             {
                 C[a + b * MATRIX_DIMENSION_XY] += A[c + b * MATRIX_DIMENSION_XY] * B[a + c * MATRIX_DIMENSION_XY];
             }
 }
-//************************************************************************************************************************
+/************************************************************************************************************************* */
 void synch(int par_id, int par_count, int *ready)
 {
     /* TODO: synch algorithm. make sure, ALL processes get stuck here until all ARE here
@@ -86,6 +89,11 @@ void synch(int par_id, int par_count, int *ready)
     }
 }
 
+float get_matrix_elem(float *M, int x, int y)
+{
+    return M[x + y * MATRIX_DIMENSION_XY];
+}
+
 void quadratic_matrix_multiplication_parallel(int par_id, int par_count, float *A, float *B, float *C, int *ready)
 {
     int x, y, z;
@@ -98,7 +106,7 @@ void quadratic_matrix_multiplication_parallel(int par_id, int par_count, float *
             float sum = 0;
             for (z = 0; z < MATRIX_DIMENSION_XY; z++)
             {
-                sum += get_matrix_elem(A, x, z) * get_matrix_elem(B, z, y);
+                sum += get_matrix_elem(A, z, y) * get_matrix_elem(B, z, y);
             }
             set_matrix_elem(C, x, y, sum);
         }
@@ -106,13 +114,16 @@ void quadratic_matrix_multiplication_parallel(int par_id, int par_count, float *
     ready[par_id] = 1;
 }
 
-//************************************************************************************************************************
+/************************************************************************************************************************* */
 int main(int argc, char *argv[])
 {
-    int par_id = 0;    // the parallel ID of this process
-    int par_count = 1; // the amount of processes
-    float *A, *B, *C;  // matrices A,B and C
-    int *ready;        // needed for synch
+    int par_id = 0;    /* the parallel ID of this process */
+    int par_count = 1; /* the amount of processes */
+    float *A, *B, *C;  /* matrices A,B and C */
+    int *ready;        /* needed for synch */
+    int fd[4];
+    float M[MATRIX_DIMENSION_XY * MATRIX_DIMENSION_XY];
+
     if (argc != 3)
     {
         printf("no shared\n");
@@ -121,17 +132,16 @@ int main(int argc, char *argv[])
     {
         par_id = atoi(argv[1]);
         par_count = atoi(argv[2]);
-        // strcpy(shared_mem_matrix,argv[3]);
+        /* strcpy(shared_mem_matrix,argv[3]); */
     }
     if (par_count == 1)
     {
         printf("only one process\n");
     }
 
-    int fd[4];
     if (par_id == 0)
     {
-        // TODO: init the shared memory for A,B,C, ready. shm_open with C_CREAT here! then ftruncate! then mmap
+        /* TODO: init the shared memory for A,B,C, ready. shm_open with C_CREAT here! then ftruncate! then mmap */
         fd[0] = shm_open("matrixA", O_CREAT | O_RDWR, 0777);
         fd[1] = shm_open("matrixB", O_CREAT | O_RDWR, 0777);
         fd[2] = shm_open("matrixC", O_CREAT | O_RDWR, 0777);
@@ -147,8 +157,8 @@ int main(int argc, char *argv[])
     }
     else
     {
-        // TODO: init the shared memory for A,B,C, ready. shm_open withOUT C_CREAT here! NO ftruncate! but yes to mmap
-        sleep(2); // needed for initalizing synch
+        /* TODO: init the shared memory for A,B,C, ready. shm_open withOUT C_CREAT here! NO ftruncate! but yes to mmap */
+        sleep(2); /* needed for initalizing synch */
         fd[0] = shm_open("matrixA", O_RDWR, 0777);
         fd[1] = shm_open("matrixB", O_RDWR, 0777);
         fd[2] = shm_open("matrixC", O_RDWR, 0777);
@@ -163,7 +173,7 @@ int main(int argc, char *argv[])
 
     if (par_id == 0)
     {
-        // TODO: initialize the matrices A and B
+        /* TODO: initialize the matrices A and B */
         int x, y;
         for (x = 0; x < MATRIX_DIMENSION_XY; x++)
             for (y = 0; y < MATRIX_DIMENSION_XY; y++)
@@ -175,7 +185,7 @@ int main(int argc, char *argv[])
 
     synch(par_id, par_count, ready);
 
-    // TODO: quadratic_matrix_multiplication_parallel(par_id, par_count,A,B,C, ...);
+    /* TODO: quadratic_matrix_multiplication_parallel(par_id, par_count,A,B,C, ...); */
     /*
      * Take a portion of the rows (not columns) and multiply the matrices A and B, placing the yield into matrix C.
      * When finished, mark as so in the ready structure
@@ -188,13 +198,12 @@ int main(int argc, char *argv[])
         quadratic_matrix_print(C);
     synch(par_id, par_count, ready);
 
-    // lets test the result:
-    float M[MATRIX_DIMENSION_XY * MATRIX_DIMENSION_XY];
+    /* lets test the result: */
     quadratic_matrix_multiplication(A, B, M);
     if (quadratic_matrix_compare(C, M))
-        print("full points!\n");
+        printf("full points!\n");
     else
-        print("buuug!\n");
+        printf("buuug!\n");
 
     close(fd[0]);
     close(fd[1]);
